7d1321085bd0931a8cbcabf87760f9290ee63eb7
msyksphinz-self <msyksphinz.dev@gmail.com>
2019-10-30 15:49:48 +0800

    Func: rewrite rumy-build/build.rb for cpp-package

--- build-rumy/build.rb
+++ build-rumy/build.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 
-load "rumy-main.rb"
+load "rumy-cpp.rb"
 
 python_cflag  = `python3.6m-config --cflags`.sub("\n", "")
 python_ldflag = `python3.6m-config --ldflags`.sub("\n", "")
@@ -18,92 +18,54 @@ compile_options = compile_options + [python_cflag]
 link_options = []
 link_options = [python_ldflag]
 
-make_target "test" do
-  global
-  depends ["test.c"]
-  executes ["gcc test.c -o test"]
-end
+link_libs = ["-lbfd", "-lpython3.6m", "-lgmp", "-lgmpxx"]
 
-obj_lists = [
-  "riscv_pe_thread.o",
-  "riscv_syscall.o",
-  "riscv_fds.o",
-  "riscv_clint.o",
-  "env.o",
-  "inst_category_riscv.o",
-  "trace.o",
-  "inst_print_riscv.o",
-  "inst_mnemonic_riscv.o",
-  "inst_riscv_init.o",
-  "inst_decoder_riscv.o",
-  "inst_riscv.o",
-  "inst_ops_riscv.o",
-  "inst_operand_riscv.o",
-  "dec_utils_riscv.o",
-  "riscv_sysreg_rw.o",
-  "riscv_sysreg_impl.o",
-  "riscv_sysreg_str.o",
-  "riscv_page_table.o",
-  "memory_block.o",
-  "mem_body.o",
-  "gdb_env.o"
+cedar_cpp_lists = [
+  "../src/riscv_pe_thread.cpp",
+  "../src/riscv_syscall.cpp",
+  "../src/riscv_fds.cpp",
+  "../src/riscv_clint.cpp",
+  "../src/env.cpp",
+  "../src/inst_category_riscv.cpp",
+  "../src/trace.cpp",
+  "../src/inst_print_riscv.cpp",
+  "../src/inst_mnemonic_riscv.cpp",
+  "../src/inst_riscv_init.cpp",
+  "../src/inst_decoder_riscv.cpp",
+  "../src/inst_riscv.cpp",
+  "../src/inst_ops_riscv.cpp",
+  "../src/inst_operand_riscv.cpp",
+  "../src/dec_utils_riscv.cpp",
+  "../src/riscv_sysreg_rw.cpp",
+  "../src/riscv_sysreg_impl.cpp",
+  "../src/riscv_sysreg_str.cpp",
+  "../src/riscv_page_table.cpp",
+  "../src/memory_block.cpp",
+  "../src/mem_body.cpp",
+  "../src/gdb_env.cpp"
 ]
 
 c_options = compile_options.join(' ').to_s
 l_options = link_options.join(' ').to_s
 
-obj_lists.each {|obj|
-  make_target obj do
-    src = "../src/" + obj.sub(".o", ".cpp")
-    depends [src]
-    executes ["g++ #{c_options} -c #{src} -o #{obj}"]
-  end
-}
-
-make_target "libriscv_cedar.a" do
-  depends obj_lists
-  executes ["ar qc libriscv_cedar.a #{obj_lists.join(' ').to_s}"]
-end
+make_library "libriscv_cedar.a", cedar_cpp_lists, compile_options, []
 
 make_target :config_hpp do
   executes ["sed 's/@VERSION@/#{build_date}/g' config.h.in |
              sed 's/@REVISION@/#{build_version}/g' > config.hpp"]
 end
 
-make_target "swimmer_main.o" do
-  src = "../src/swimmer_main.cpp"
-  depends [:config_hpp]
-  executes ["g++ #{c_options} -c #{src} -o swimmer_main.o"]
-end
-
-swimmer_obj_lists = [
-  "swimmer_util.o",
-  "riscv_bfd_env.o",
-  "python3_env.o"
+swimmer_cpp_lists = [
+  "../src/swimmer_main.cpp",
+  "../src/swimmer_util.cpp",
+  "../src/riscv_bfd_env.cpp",
+  "../src/python3_env.cpp"
 ]
 
 
-swimmer_obj_lists.each {|obj|
-  make_target obj do
-    src = "../src/" + obj.sub(".o", ".cpp")
-    depends [src]
-    executes ["g++ #{c_options} -c #{src} -o #{obj}"]
-  end
-}
-
-
-make_target "swimmer_riscv" do
-  global
-  depends swimmer_obj_lists + ["swimmer_main.o"] + ["libriscv_cedar.a"]
-  link_libs = ["-lbfd", "-lpython3.6m", "-lgmp", "-lgmpxx"].join(' ').to_s
-  executes ["g++ #{l_options} #{swimmer_obj_lists.join(' ').to_s} \
-            swimmer_main.o \
-            libriscv_cedar.a \
-            ../vendor/softfloat/build/libsoftfloat.a \
-            -o swimmer_riscv \
-            #{link_libs}"]
-end
-
+make_execute("swimmer_riscv", swimmer_cpp_lists, ["libriscv_cedar.a", "../vendor/softfloat/build/libsoftfloat.a"],
+             compile_options, link_options,
+             link_libs)
 
 if ARGV.length == 0 then
   exec_target "swimmer_riscv"
